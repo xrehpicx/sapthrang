7f448c0a485bd360fdccf10dd749f9c2295c3aae
Raj Sharma <raj.fps2000@gmail.com>
2022-05-22 14:03:59 +0530

    feat: basic header

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,6 @@
 /** @jsxImportSource @emotion/react */
-import { Container, css, Typography } from "@mui/material";
+import { css, Typography } from "@mui/material";
+import { motion } from "framer-motion";
 import { Background } from "./background";
 
 export function Home() {
@@ -10,24 +11,42 @@ export function Home() {
   );
 }
 
+const MTypography = motion(Typography);
 function Header() {
   return (
-    <div
-      css={css`
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        height: 100vh;
-      `}
-    >
+    <header>
       <Background />
-      <Typography
-        variant="h1"
-        color="text.primary"
-        fontFamily={"'Montez', cursive"}
+      <div
+        css={css`
+          display: flex;
+          align-items: center;
+          flex-direction: column;
+          justify-content: center;
+          height: 100vh;
+          backdrop-filter: blur(1rem);
+        `}
       >
-        Sapthrang
-      </Typography>
-    </div>
+        <MTypography
+          variant="h1"
+          color="text.primary"
+          fontFamily={"'Montez', cursive"}
+          initial={{
+            opacity: 0,
+            textShadow: `none`,
+          }}
+          animate={{
+            opacity: 1,
+            textShadow: `0 0 7px #fff, 0 0 10px #fff, 0 0 21px #fff,
+            0 0 42px #ffee00, 0 0 82px #fff700, 0 0 92px #ff0000,
+            0 0 102px #ffd000`,
+          }}
+        >
+          Sapthrang
+          <Typography variant="body1" component={"p"}>
+            {new Date().getFullYear()}
+          </Typography>
+        </MTypography>
+      </div>
+    </header>
   );
 }
--- src/components/background.tsx
+++ src/components/background.tsx
@@ -2,7 +2,7 @@
 import { css } from "@mui/material";
 import { useEffect, useRef } from "react";
 // @ts-ignore
-import StarfieldAnimation from "react-starfield-animation";
+// import StarfieldAnimation from "react-starfield-animation";
 // @ts-ignore
 import BIRDS from "vanta/dist/vanta.birds.min";
 
@@ -14,7 +14,7 @@ export function Background() {
       el: ref.current,
       mouseControls: true,
       touchControls: true,
-      gyroControls: false,
+      gyroControls: true,
       minHeight: 200.0,
       minWidth: 200.0,
       scale: 1.0,
@@ -25,10 +25,10 @@ export function Background() {
       colorMode: "lerp",
       birdSize: 2.0,
       wingSpan: 24.0,
-      speedLimit: 10.0,
+      speedLimit: 20.0,
       separation: 45.0,
-      alignment: 1.0,
-      cohesion: 9.0,
+      alignment: 5.0,
+      cohesion: 1.0,
       quantity: 3.0,
     });
 
