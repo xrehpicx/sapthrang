b295a1dd3842ce8f49dbf620418a58da58efc285
Raj Sharma <raj.fps2000@gmail.com>
2022-05-22 16:05:43 +0530

    test

--- api/notion/index.js
+++ api/notion/index.js
@@ -1,5 +1,5 @@
 import { NotionAPI } from "notion-client";
-import { VercelRequest, VercelResponse } from "@vercel/node";
+// import { VercelRequest, VercelResponse } from "@vercel/node";
 /* import axios from "axios";
 // request -> ?id={your_id}
 axios.defaults.baseURL =
@@ -8,8 +8,8 @@ axios.defaults.baseURL =
     : "http://localhost:3000/"; */
 
 const notion = new NotionAPI();
-async function handler(req: VercelRequest, res: VercelResponse) {
-  const { id } = req.query as { id: string };
+async function handler(req, res) {
+  const { id } = req.query;
   if (id) {
     res.json(await notion.getPage(id));
     return;
